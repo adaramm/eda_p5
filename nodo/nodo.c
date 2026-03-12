5ca12956438abd93f279823df97523e64e6a9d23
adaramm <ariel.mercado@ingenieria.unam.edu>
2026-03-12 06:17:41 -0600

    Add function placeholders in nodo.c
    Added placeholders for 'nodo_crear' and 'nodo_destruir' functions.

--- nodo/nodo.c
+++ nodo/nodo.c
@@ -3,18 +3,9 @@
 
 Nodo* nodo_crear(int dato)
 {
-    Nodo* nuevo = malloc(sizeof(Nodo));
-
-    if(nuevo != NULL)
-    {
-        nuevo->dato = dato;
-        nuevo->siguiente = NULL;
-    }
-
-    return nuevo;
-}
+    // Todo: implementar la función }
 
 void nodo_destruir(Nodo* nodo)
 {
-    free(nodo);
+    // Todo: implementar la función 
 }
